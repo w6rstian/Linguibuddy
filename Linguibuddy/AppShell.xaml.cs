e8a2a736c08b00ff7416eaffa1b82f76ed3e4995
w6rstian <quarennc@gmail.com>
2025-10-29 00:07:25 +0100

    Add project files.

---
+++ Linguibuddy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Linguibuddy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Linguibuddy/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Linguibuddy")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Linguibuddy.Pages")]
