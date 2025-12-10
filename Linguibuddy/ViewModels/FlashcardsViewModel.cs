5b3297d98ff2f89820befb00aeac82c4f595704a
szymanowsk <szymanowskakam@gmail.com>
2025-12-10 17:34:49 +0100

    fiszki init (przykładowe, brak połączenia z realną bazą słów)

--- Linguibuddy/ViewModels/FlashcardsViewModel.cs
+++ Linguibuddy/ViewModels/FlashcardsViewModel.cs
@@ -1,14 +1,55 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Linguibuddy.Models;
+using System.Collections.Generic;
 
 namespace Linguibuddy.ViewModels
 {
     public partial class FlashcardsViewModel : ObservableObject
     {
+        private Queue<Flashcard> _flashcardsQueue;
 
+        [ObservableProperty]
+        private Flashcard currentFlashcard;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsLearning))]
+        private bool isFinished;
+
+        public bool IsLearning => !IsFinished;
+
+        public FlashcardsViewModel()
+        {
+            LoadData();
+            NextCard();
+        }
+
+        private void LoadData()
+        {
+            // Przyk³adowe dane
+            var data = new List<Flashcard>
+            {
+                new Flashcard { Word = "Serendipity", Translation = "Szczêœliwy traf", PartOfSpeech = "noun", ExampleSentence = "It was pure serendipity that we met." },
+                new Flashcard { Word = "To develop", Translation = "Rozwijaæ (siê)", PartOfSpeech = "verb", ExampleSentence = "He wants to develop his skills." },
+                new Flashcard { Word = "Resilient", Translation = "Odporny", PartOfSpeech = "adjective", ExampleSentence = "Bamboo is a resilient material." }
+            };
+
+            _flashcardsQueue = new Queue<Flashcard>(data);
+        }
+
+        [RelayCommand]
+        public void NextCard()
+        {
+            if (_flashcardsQueue.Count > 0)
+            {
+                CurrentFlashcard = _flashcardsQueue.Dequeue();
+                IsFinished = false;
+            }
+            else
+            {
+                CurrentFlashcard = null;
+                IsFinished = true;
+            }
+        }
     }
 }
