5ca18be61696341e9b998bd370d28ec83807064b
szymanowsk <szymanowskakam@gmail.com>
2025-12-12 02:28:53 +0100

    Models update

--- Linguibuddy/Models/Flashcard.cs
+++ Linguibuddy/Models/Flashcard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,16 @@ namespace Linguibuddy.Models
     public class Flashcard
     {
         public int Id { get; set; }
-        public string Word { get; set; }
-        public string Translation { get; set; }
-        public string PartOfSpeech { get; set; }
-        public string ExampleSentence { get; set; }
+        // [MaxLength(wartoœæ)] - tylko do odczytu w bazie danych, tylko maksymalna d³ugoœæ znaków
+        // [StringLength(wartoœæ, ErrorMessage = "Komunikat o b³êdzie] - do odczytu i zapisu w bazie danych, maksymalna d³ugoœæ znaków z komunikatem o b³êdzie, mo¿liwoœæ dania minimalnej d³ugoœci
+        // {0} = Property Name, {1} = Max Length, {2} = Min Length
+        [StringLength(50, ErrorMessage = "{0} can have a max of {1} characters")]
+        public string Word { get; set; } = string.Empty;
+        [StringLength(128, ErrorMessage = "{0} can have a max of {1} characters")]
+        public string Translation { get; set; } = string.Empty;
+        [StringLength(50, ErrorMessage = "{0} can have a max of {1} characters")]
+        public string PartOfSpeech { get; set; } = string.Empty;
+        [StringLength(512, ErrorMessage = "{0} can have a max of {1} characters")]
+        public string ExampleSentence { get; set; } = string.Empty;
     }
 }
