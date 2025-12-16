a46cf36f4ebcc05ebc8ece9fb9275b30a9d60514
szymanowsk <szymanowskakam@gmail.com>
2025-12-16 19:39:12 +0100

    Revert "tab update"
    This reverts commit a0ed5725cabf7e644d7f17ed03b2e480385992d9.

--- Linguibuddy/AppShell.xaml.cs
+++ Linguibuddy/AppShell.xaml.cs
@@ -1,14 +1,10 @@
-﻿using Linguibuddy.Views;
-
-namespace Linguibuddy
+﻿namespace Linguibuddy
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
-
-            Routing.RegisterRoute(nameof(FlashcardsPage), typeof(FlashcardsPage));
         }
     }
 }
