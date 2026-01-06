fab5187bdbad1666871f3e263bf394d69fac86d2
w6rstian <quarennc@gmail.com>
2026-01-06 15:21:16 +0100

    Modele do osiągnięć

---
+++ Linguibuddy/Models/Achievement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Models
+{
+    public class Achievement
+    {
+        public int Id;
+        public string Name = string.Empty;
+        public string Description = string.Empty;
+        public string IconUrl = string.Empty;
+        public string Crieria = string.Empty; // Jakieś warunki odblokowania w Json
+    }
+}
---
+++ Linguibuddy/Models/UserAchievement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Models
+{
+    public class UserAchievement
+    {
+        public int Id { get; set; }
+        public int AchievementId { get; set; }
+        public bool IsUnlocked { get; set; }
+        public DateTime UnlockDate { get; set; }
+
+        // public float progress { get; set;}
+    }
+}
