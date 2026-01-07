7a78908756fdf2db1c24d11f692aec23d56e4348
w6rstian <quarennc@gmail.com>
2026-01-07 11:48:12 +0100

    DB stuff

--- Linguibuddy/Models/UserAchievement.cs
+++ Linguibuddy/Models/UserAchievement.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Firebase.Auth;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,14 @@ namespace Linguibuddy.Models
 {
     public class UserAchievement
     {
+        [Key]
         public int Id { get; set; }
+        public string AppUserId { get; set; }
+        [ForeignKey(nameof(AppUserId))]
+        public AppUser AppUser { get; set; }
         public int AchievementId { get; set; }
+        [ForeignKey(nameof(AchievementId))]
+        public Achievement Achievement { get; set; }
         public bool IsUnlocked { get; set; }
         public DateTime UnlockDate { get; set; }
 
