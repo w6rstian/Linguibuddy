8b068be3eb98e675969071ec5456e6b41e8230af
w6rstian <quarennc@gmail.com>
2026-01-13 13:45:41 +0100

    Requirements commented out for now

--- Linguibuddy/Models/Achievement.cs
+++ Linguibuddy/Models/Achievement.cs
@@ -10,7 +10,7 @@ namespace Linguibuddy.Models
         public string Description { get; set; } = string.Empty;
         public string IconUrl { get; set; } = string.Empty;
         public string LockedIconUrl { get; set; } = "lock_100dp_light.png";
-        public Dictionary<string, object> Requirements { get; set; } = [];
+        //public Dictionary<string, object> Requirements { get; set; } = [];
         public ICollection<UserAchievement> UserAchievements { get; set; } = new List<UserAchievement>();
     }
 }
