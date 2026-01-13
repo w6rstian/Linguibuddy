73acc3bf6a5cecb94ad8acbda5a68ac87ff16ab9
w6rstian <quarennc@gmail.com>
2026-01-13 16:15:44 +0100

    AppUser Repo + Serwis

---
+++ Linguibuddy/Repositories/AppUserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Linguibuddy.Data;
+using Linguibuddy.Interfaces;
+using Linguibuddy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Repositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly DataContext _db;
+
+        public AppUserRepository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<AppUser?> GetByIdAsync(string userId)
+            => await _db.AppUsers
+            .Where(au => au.Id == userId)
+            .FirstAsync();
+
+        public async Task AddAsync(AppUser user)
+        {
+            await _db.AppUsers.AddAsync(user);
+            await _db.SaveChangesAsync();
+        }
+
+
+        public async Task SaveChangesAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ Linguibuddy/Services/AppUserService.cs
@@ -0,0 +1,32 @@
+﻿using Linguibuddy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Services
+{
+    public class AppUserService
+    {
+        private readonly IAppUserRepository _appUsers;
+
+        public AppUserService(IAppUserRepository appUsers)
+        {
+            _appUsers = appUsers;
+        }
+
+        public async Task AddPointsAsync(string userId, int points)
+        {
+            var user = await _appUsers.GetByIdAsync(userId)
+                ?? throw new Exception("User not found");
+
+            user.Points += points;
+
+            await _appUsers.SaveChangesAsync();
+        }
+
+        //public async Task SetBestLearningStreakAsync(string userId, int )
+        //public async Task<int> GetUserPoints(string userId);
+    }
+}
