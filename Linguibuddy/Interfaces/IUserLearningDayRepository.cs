f1d10770331d575c4ed6674d8b725d9a23aa9753
w6rstian <quarennc@gmail.com>
2026-01-13 15:02:48 +0100

    Repository for getting learning days

---
+++ Linguibuddy/Interfaces/IUserLearningDayRepository.cs
@@ -0,0 +1,16 @@
+﻿using Linguibuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Interfaces
+{
+    public interface IUserLearningDayRepository
+    {
+        Task<bool> ExistsAsync(string userId, DateTime date);
+        Task AddAsync(UserLearningDay day);
+        Task<List<DateTime>> GetLearningDatesAsync(string userId);
+    }
+}
---
+++ Linguibuddy/Repositories/UserLearningDayRepository.cs
@@ -0,0 +1,39 @@
+﻿using Linguibuddy.Data;
+using Linguibuddy.Interfaces;
+using Linguibuddy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linguibuddy.Repositories
+{
+    public class UserLearningDayRepository : IUserLearningDayRepository
+    {
+        private readonly DataContext _db;
+
+        public UserLearningDayRepository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public Task<bool> ExistsAsync(string userId, DateTime date)
+            => _db.UserLearningDays
+            .AnyAsync(uld => uld.AppUserId == userId && uld.Date == date);
+
+        public async Task AddAsync(UserLearningDay day)
+        {
+            _db.UserLearningDays.Add(day);
+            await _db.SaveChangesAsync();
+        }
+
+        public Task<List<DateTime>> GetLearningDatesAsync(string userId)
+            => _db.UserLearningDays
+            .Where(uld => uld.AppUserId == userId)
+            .OrderByDescending(uld => uld.Date)
+            .Select(uld => uld.Date)
+            .ToListAsync();
+    }
+}
