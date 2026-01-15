f689e72c5657a8bfdfdd56a1c33d9cd5595d5390
szymanowsk <szymanowskakam@gmail.com>
2026-01-15 01:50:40 +0100

    Repositories tests

---
+++ Linguibuddy.Tests/RepositoryTests/AppUserRepositoryTests.cs
@@ -0,0 +1,109 @@
+using FluentAssertions;
+using Linguibuddy.Data;
+using Linguibuddy.Models;
+using Linguibuddy.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linguibuddy.Tests.RepositoryTests;
+
+public class AppUserRepositoryTests : IDisposable
+{
+    private readonly DataContext _context;
+    private readonly AppUserRepository _sut;
+
+    public AppUserRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new DataContext(options);
+        _sut = new AppUserRepository(_context);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnUser_WhenUserExists()
+    {
+        // Arrange
+        var userId = "user123";
+        var user = new AppUser { Id = userId, Points = 100 };
+        _context.AppUsers.Add(user);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _sut.GetByIdAsync(userId);
+
+        // Assert
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(userId);
+        result.Points.Should().Be(100);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnNull_WhenUserDoesNotExist()
+    {
+        // Act
+        var result = await _sut.GetByIdAsync("nonexistent");
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldAddUserAndSaveToDb()
+    {
+        // Arrange
+        var user = new AppUser { Id = "newUser", Points = 50 };
+
+        // Act
+        await _sut.AddAsync(user);
+
+        // Assert
+        var savedUser = await _context.AppUsers.FindAsync("newUser");
+        savedUser.Should().NotBeNull();
+        savedUser!.Points.Should().Be(50);
+    }
+
+    [Fact]
+    public async Task Update_ShouldMarkEntityAsModified()
+    {
+        // Arrange
+        var user = new AppUser { Id = "user1", Points = 10 };
+        _context.AppUsers.Add(user);
+        await _context.SaveChangesAsync();
+        _context.ChangeTracker.Clear();
+
+        // Act
+        user.Points = 20;
+        _sut.Update(user);
+
+        // Assert
+        _context.Entry(user).State.Should().Be(EntityState.Modified);
+    }
+
+    [Fact]
+    public async Task SaveChangesAsync_ShouldPersistChangesToDb()
+    {
+        // Arrange
+        var user = new AppUser { Id = "user1", Points = 10 };
+        _context.AppUsers.Add(user);
+        await _context.SaveChangesAsync();
+
+        user.Points = 20;
+        _sut.Update(user);
+
+        // Act
+        await _sut.SaveChangesAsync();
+
+        // Assert
+        _context.ChangeTracker.Clear();
+        var savedUser = await _context.AppUsers.FindAsync("user1");
+        savedUser!.Points.Should().Be(20);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+}
---
+++ Linguibuddy.Tests/RepositoryTests/UserLearningDayRepositoryTests.cs
@@ -0,0 +1,125 @@
+using FluentAssertions;
+using Linguibuddy.Data;
+using Linguibuddy.Models;
+using Linguibuddy.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linguibuddy.Tests.RepositoryTests;
+
+public class UserLearningDayRepositoryTests : IDisposable
+{
+    private readonly DataContext _context;
+    private readonly UserLearningDayRepository _sut;
+
+    public UserLearningDayRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new DataContext(options);
+        _sut = new UserLearningDayRepository(_context);
+    }
+
+    [Fact]
+    public async Task ExistsAsync_ShouldReturnTrue_WhenRecordExists()
+    {
+        // Arrange
+        var userId = "user1";
+        var date = DateTime.Today;
+        _context.UserLearningDays.Add(new UserLearningDay { AppUserId = userId, Date = date });
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _sut.ExistsAsync(userId, date);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ExistsAsync_ShouldReturnFalse_WhenRecordDoesNotExist()
+    {
+        // Arrange
+        var userId = "user1";
+        var date = DateTime.Today;
+
+        // Act
+        var result = await _sut.ExistsAsync(userId, date);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldAddRecordAndSaveToDb()
+    {
+        // Arrange
+        var day = new UserLearningDay { AppUserId = "user1", Date = DateTime.Today };
+
+        // Act
+        await _sut.AddAsync(day);
+
+        // Assert
+        var savedDay = await _context.UserLearningDays.FirstOrDefaultAsync(d => d.AppUserId == "user1");
+        savedDay.Should().NotBeNull();
+        savedDay!.Date.Should().Be(DateTime.Today);
+    }
+
+    [Fact]
+    public async Task GetLearningDatesAsync_ShouldReturnDatesForSpecificUser()
+    {
+        // Arrange
+        var userId1 = "user1";
+        var userId2 = "user2";
+        var date1 = DateTime.Today;
+        var date2 = DateTime.Today.AddDays(-1);
+        
+        _context.UserLearningDays.AddRange(
+            new UserLearningDay { AppUserId = userId1, Date = date1 },
+            new UserLearningDay { AppUserId = userId1, Date = date2 },
+            new UserLearningDay { AppUserId = userId2, Date = date1 }
+        );
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _sut.GetLearningDatesAsync(userId1);
+
+        // Assert
+        result.Should().HaveCount(2);
+        result.Should().Contain(date1);
+        result.Should().Contain(date2);
+        result.Should().NotContain(d => d == DateTime.Today.AddDays(1)); // Random date not in db
+    }
+
+    [Fact]
+    public async Task GetLearningDatesAsync_ShouldReturnDatesOrderedDescending()
+    {
+        // Arrange
+        var userId = "user1";
+        var date1 = DateTime.Today.AddDays(-2);
+        var date2 = DateTime.Today;
+        var date3 = DateTime.Today.AddDays(-1);
+
+        _context.UserLearningDays.AddRange(
+            new UserLearningDay { AppUserId = userId, Date = date1 },
+            new UserLearningDay { AppUserId = userId, Date = date2 },
+            new UserLearningDay { AppUserId = userId, Date = date3 }
+        );
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await _sut.GetLearningDatesAsync(userId);
+
+        // Assert
+        result.Should().BeInDescendingOrder();
+        result.First().Should().Be(date2);
+        result.Last().Should().Be(date1);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+}
